a7a0d3fe6c4cc5eb4c6f5dacdb51f5f841b7f9bd
Lan Anh <dhlananh2309@gmail.com>
2025-05-19 21:36:31 +0700

    refactor: remove unused env variables

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ load_dotenv()
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host=os.getenv("DB_HOST", "localhost"),
+            host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME"),
