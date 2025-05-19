3d3e30d7fd4459c3e05c4814a34a8b5e4047f4c1
dhlananhh <dhlananh2309@gmail.com>
2025-05-19 23:06:07 +0700

    feat: enable CORS for all origins

--- app/main.py
+++ app/main.py
@@ -5,6 +5,14 @@ from app.routes import router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router, prefix="/api")
 
